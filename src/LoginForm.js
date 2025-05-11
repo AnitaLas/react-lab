22add72b670185a3aebf7c92985aa63733bff25d
laszczak <anita.laszczak@gmail.com>
2025-05-11 13:25:37 +0200

    login form

---
+++ src/LoginForm.js
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+export function LoginForm(props) {
+
+    const [email, setEmail] = useState('');
+
+
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+
+    return (<div>
+        <div>Zaloguj się e-mailem</div>
+        <input type="text" value={email} onChange={handleChange}/>
+        <button onClick={() => props.onLogin(email)}> Wchodzę</button>
+    </div>);
+}
