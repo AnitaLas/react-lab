2f9374fda6689ff1afd7cf2871e00cdb3c1bc011
laszczak <anita.laszczak@gmail.com>
2025-05-11 14:08:46 +0200

    dodawanie zajęć v2

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -1,8 +1,11 @@
+import MeetingsPage from "./meetings/MeetingsPage";
+
 export function UserPanel(props) {
     return (
         <div>
             <div> Witaj {props.userName} </div>
             <button  onClick={() => props.onLogout()}> Wyloguj</button>
+            <MeetingsPage/>
         </div>
 
     );
